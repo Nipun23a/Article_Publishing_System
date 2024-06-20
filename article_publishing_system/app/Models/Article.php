aca9dea5162bef0ea1bd87e4ce4bb3d23e7aa11c
Nipuna Kaweya <nipuna609@gmail.com>
2024-06-20 13:51:08 +0530

    Article Modeld Updated

--- article_publishing_system/app/Models/Article.php
+++ article_publishing_system/app/Models/Article.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'article_title',
+        'article_content',
+        'author_id',
+    ];
+
+    public function author(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
