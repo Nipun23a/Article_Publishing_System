00b75d7d65e7458504aeb83370d24c18dd2bb851
Nipuna Kaweya <nipuna609@gmail.com>
2024-06-20 18:08:41 +0530

    Register Controller is Fixed

--- article_publishing_system/app/Http/Controllers/Auth/RegisterController.php
+++ article_publishing_system/app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -49,24 +50,43 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'user_name' => ['required', 'string', 'max:255'],
+            'user_email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'user_password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
+
+
 
     /**
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return User
      */
-    protected function create(array $data)
+    protected function create(array $data): User
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'user_name' => $data['user_name'],
+            'user_email' => $data['user_email'],
+            'user_password' => Hash::make($data['user_password']),
+            'user_role' => 2,
+            'user_pic' => "New",
+            'user_bio' => "New"
         ]);
+    }
+
+    protected function registered(Request $request, $user)
+    {
+        return redirect($this->redirectPath())->with('success', 'Registration successful!');
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        return redirect()->back()
+            ->withInput($request->only('email', 'remember'))
+            ->withErrors([
+                'email' => trans('auth.failed'),
+            ]);
     }
 }
