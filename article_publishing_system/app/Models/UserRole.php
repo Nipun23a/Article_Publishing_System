19d39ed801784701b8c536e6bc0fff23bbae8da7
Nipuna Kaweya <nipuna609@gmail.com>
2024-06-20 13:49:09 +0530

    UserRole Updated

--- article_publishing_system/app/Models/UserRole.php
+++ article_publishing_system/app/Models/UserRole.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserRole extends Model
 {
     use HasFactory;
+    protected $fillable = ['role_name'];
+
+    public function users(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this -> hasMany(User::class,'user_role');
+    }
 }
