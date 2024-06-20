a5a27f8a82f7a2682998799af7e78d09713d7510
Nipuna Kaweya <nipuna609@gmail.com>
2024-06-20 13:43:12 +0530

    home route added

--- article_publishing_system/routes/web.php
+++ article_publishing_system/routes/web.php
@@ -26,3 +26,7 @@ Route::get('/db-test', function () {
         return 'Could not connect to the database. Error: ' . $e->getMessage();
     }
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
