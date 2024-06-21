436aa11be052d565319bfcfa886e90502550115a
Nipuna Kaweya <nipuna609@gmail.com>
2024-06-21 13:03:02 +0530

    Edit blade upadeted for show email,username, fille handling implemented

--- article_publishing_system/resources/views/profile/edit.blade.php
+++ article_publishing_system/resources/views/profile/edit.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/profile/edit.blade.php -->
 @extends('layouts.app')
 
 @section('content')
@@ -15,14 +14,14 @@
                             </div>
                         @endif
 
-                        <form method="POST" action="{{ route('profile.update') }}">
+                        <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
                             @csrf
 
                             <div class="form-group row">
                                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $user->name) }}" required autofocus>
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="user_name" value="{{ old('user_name', $user->user_name) }}" required autofocus>
 
                                     @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -47,7 +46,21 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label for="current-password" class="col-md-4 col-form-label text-md-right">{{ __('Current Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="current-password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="user_current_password" required>
+
+                                    @error('current_password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
@@ -68,6 +81,19 @@
                                 </div>
                             </div>
 
+                            <div class="form-group row">
+                                <label for="user_pic" class="col-md-4 col-form-label text-md-right">{{ __('Profile Picture') }}</label>
+                                <div class="col-md-6">
+                                    <input id="user_pic" type="file" class="form-control-file @error('user_pic') is-invalid @enderror" name="user_pic" accept="image/*">
+                                    @error('user_pic')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                    <img id="user_pic_output" class="img-thumbnail mt-2" style="display: none; max-width: 200px;">
+                                </div>
+                            </div>
+
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
@@ -81,5 +107,8 @@
             </div>
         </div>
     </div>
+
+    <script src="{{asset('js/profile.js')}}"></script>
 @endsection
+
 
